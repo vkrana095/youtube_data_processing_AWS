7a0ff989459ad6fe2cd1016698fbffe13cb41fe2
Vivek Rana <vivekrana095@gmail.com>
2024-09-21 21:43:35 +0530

    Update pyspark_code.py

--- pyspark_code.py
+++ pyspark_code.py
@@ -24,7 +24,8 @@ job.init(args['JOB_NAME'], args)
 ############################### Added by Darshil ###############################
 predicate_pushdown = "region in ('ca','gb','us')"
 
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "de_youtube_crawl_database", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+
 
 ## @type: ApplyMapping
 ## @args: [mapping = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1"]
@@ -51,7 +52,10 @@ dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ct
 
 datasink1 = dropnullfields3.toDF().coalesce(1)
 df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-youtube-cleansed-csv-parquet/youtube/raw_statiistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+
+# /AmazonS3_node1726902480948 = glueContext.write_dynamic_frame.from_options(frame=AWSGlueDataCatalog_node1726902320173, connection_type="s3", format="glueparquet", connection_options={"path": "s3://de-youtube-cleansed-csv-parquet/youtube/raw_statiistics/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1726902480948")
+
 
 ############################### Added by Darshil ###############################
 job.commit()
